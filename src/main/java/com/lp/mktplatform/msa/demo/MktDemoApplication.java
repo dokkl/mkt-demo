001a754f24f2ce230a0de989837584af84afeb53
hoon <dokkl@naver.com>
2022-01-23 20:47:12 +0900

    init

---
+++ src/main/java/com/lp/mktplatform/msa/demo/MktDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lp.mktplatform.msa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MktDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lp/mktplatform/msa/demo/MktDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.mktplatform.msa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MktDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
